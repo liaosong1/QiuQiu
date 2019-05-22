a338130933fd97ebf76feb0b7ff9ffefece59600
liaosong <1056838611@qq.com>
2019-05-22 21:00:00 +0800

    qiuqiu

---
+++ qiuqiu/src/main/java/com/qiuqiu/QiuqiuApplication.java
@@ -0,0 +1,13 @@
+package com.qiuqiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuqiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiuqiuApplication.class, args);
+    }
+
+}
